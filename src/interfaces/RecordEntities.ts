4afd61e268bfd01c2e1951707102712b66dae6f7
Darcio Vilela <darcio@jp7.com.br>
2022-09-16 17:46:56 -0300

    Interfaces

---
+++ src/interfaces/RecordEntities.ts
@@ -0,0 +1,17 @@
+export interface Record {
+    id?: number;
+}
+
+export interface Article extends Record {
+    id?: number;
+    title: string;
+    text: string;
+    author?: Author | null;
+}
+
+export interface Author extends Record {
+    id?: number;
+    name: string;
+    articles?: Article[];
+
+}
